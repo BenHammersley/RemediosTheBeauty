43cfb556585e2b65f5171de6a0271be423d3b8b3
Ben Hammersley <ben@dangerousprecedent.com>
2013-10-10 15:43:16 +0200

    Initial Sketch to test the GPS unit
    Code from
    http://learn.adafruit.com/adafruit-ultimate-gps/direct-computer-wiring
    to test the GPS unit via the Arduino Duemilanove's serial USB connection

---
+++ sketch_oct10a/sketch_oct10a.ino
@@ -0,0 +1,15 @@
+// Remedios The Beauty
+
+// the following code taken from http://learn.adafruit.com/adafruit-ultimate-gps/direct-computer-wiring
+
+// this sketch will allow you to bypass the Atmega chip
+// and connect the fingerprint sensor directly to the USB/Serial
+// chip converter.
+ 
+// Connect VIN to +5V
+// Connect GND to Ground
+// Connect GPS RX (data into GPS) to Digital 0
+// Connect GPS TX (data out from GPS) to Digital 1
+ 
+void setup() {}
+void loop() {}
